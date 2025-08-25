7acebf44604e626c5ec57f115faaba1cdf71c7f8
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-08-26 02:05:05 +0530

    Not Important

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,13 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
