9e23e650368ae9d43c196f86555c50191a8a751f
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-08-29 16:10:50 +0530

    Removed: classname from the auth button

--- src/components/forms/AuthButton.tsx
+++ src/components/forms/AuthButton.tsx
@@ -1,14 +1,13 @@
 import { AuthListType } from '@/data/constants/variables';
 import { signIn } from 'next-auth/react';
 
-export default function AuthButton({ Icon, label, className }: AuthListType) {
+export default function AuthButton({ Icon, label }: AuthListType) {
     const handleLogin = () => signIn(label.toLowerCase());
     return (
         <button
             onClick={handleLogin}
             className={
-                'border-dark-muted/15 flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-dark text-sm font-medium shadow-sm shadow-dark-muted ' +
-                className
+                'border-dark-muted/15 flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-dark text-sm font-medium shadow-sm shadow-dark-muted'
             }
         >
             <Icon className="text-lg" /> {label}
