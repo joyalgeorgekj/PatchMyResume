a340245010de28154d94acfe7fdfc8496472e0f0
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-08-29 16:03:09 +0530

    Created: Hook to check if the path in forbidden path

---
+++ src/hooks/useHiddenRoutes.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+export const useHiddenRoutes = () => {
+    const currentPath = usePathname();
+    const forbidden: string[] = ['/login', '/register', '/signin']
+    return !forbidden.includes(currentPath);
+}
