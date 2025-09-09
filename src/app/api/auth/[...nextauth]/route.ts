2339f3b673eb9f0a8160c9fc6a0f82600254e7eb
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-09 19:31:02 +0530

    Added: Server Handle signup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,25 +5,38 @@ import GitHubProvider from 'next-auth/providers/github';
 import LinkedInProvider from 'next-auth/providers/linkedin';
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-        AppleProvider({
-            clientId: process.env.APPLE_CLIENT_ID as string,
-            clientSecret: process.env.APPLE_CLIENT_SECRET as string,
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-        }),
-        LinkedInProvider({
-            clientId: process.env.LINKEDIN_CLIENT_ID as string,
-            clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    AppleProvider({
+      clientId: process.env.APPLE_CLIENT_ID as string,
+      clientSecret: process.env.APPLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID as string,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      // attach user id (sub) to session
+      if (token.sub && session.user) session.user.id = token.sub;
+      return session;
+    },
+    async jwt({ token, account, profile }) {
+      // you can persist provider info in token
+      if (account) {
+        token.provider = account.provider;
+      }
+      return token;
+    },
+  },
 });
-
 export { handler as GET, handler as POST };
