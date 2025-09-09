4bec0bc5603151512533e77f8a8c5536f248cc18
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-09 19:48:45 +0530

    Started to impliment route restriction

--- src/hooks/useHiddenRoutes.tsx
+++ src/hooks/useHiddenRoutes.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { usePathname } from "next/navigation";
+import { usePathname } from 'next/navigation';
 
 export const useHiddenRoutes = () => {
     const currentPath = usePathname();
-    const forbidden: string[] = ['/login', '/register', '/signin']
+    const forbidden: string[] = ['/login', '/register', '/signin'];
     return !forbidden.includes(currentPath);
-}
+};
