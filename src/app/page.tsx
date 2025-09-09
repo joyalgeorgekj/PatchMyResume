dfa44585f1bdaeff2e8ffefd3f82345ef658c35b
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-09 19:25:55 +0530

    Added: Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import StepperForm from '@/components/stepper/StepperForm';
-import Feature from '@/components/ui/landing/Feature';
+"use client";
+
 import HowItWorks from '@/components/ui/landing/HowItWorks';
+import Feature from '@/components/ui/landing/Feature';
 import Main from '@/components/ui/landing/Main';
 
 export default function LandingPage() {
+
     return (
         <div className="bg-light text-default-foreground flex flex-col">
-            {/* <Main />
+            <Main />
             <Feature />
-            <HowItWorks /> */}
-
-            <StepperForm />
+            <HowItWorks />
         </div>
     );
 }
