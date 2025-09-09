2fb18fd83d8b9fc795404b63441f463fbd0771eb
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-09 19:45:32 +0530

    Created and Modified: Theme Button to add system theme and proper theme switching and icons

--- src/components/layout/ThemeButton.tsx
+++ src/components/layout/ThemeButton.tsx
@@ -3,20 +3,22 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaCircleHalfStroke } from 'react-icons/fa6';
 
 export default function ThemeButton() {
-    const { theme, setTheme } = useTheme();
-    const [mount, setMount] = useState<boolean>(false);
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-    useEffect(() => setMount(true), []);
+  useEffect(() => setMounted(true), []);
 
-    if (mount)
-        return (
-            <button
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                className="border-primary-muted text-primary flex w-full cursor-pointer items-center justify-center gap-2 rounded-full border-2 px-3 py-2 text-sm font-medium shadow-sm transition-all hover:scale-105"
-            >
-                {theme === 'dark' ? <FaMoon /> : <FaSun />}
-            </button>
-        );
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      className="flex h-9 w-9 items-center justify-center rounded-full shadow shadow-dark/25 text-dark/50 hover:scale-105 transition cursor-pointer"
+    >
+      {theme === 'dark' ? <FaMoon /> : theme === 'light' ? <FaSun /> : <FaCircleHalfStroke />}
+    </button>
+  );
 }
