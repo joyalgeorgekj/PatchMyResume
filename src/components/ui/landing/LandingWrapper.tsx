57ac7229d6ef2e82e6bd7578fc3f32e3c254d91f
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-09 19:46:55 +0530

    Added: Session Provider to access session on routes and components

--- src/components/ui/landing/LandingWrapper.tsx
+++ src/components/ui/landing/LandingWrapper.tsx
@@ -1,17 +1,18 @@
-"use client";
+'use client';
 
 import { useHiddenRoutes } from '@/hooks/useHiddenRoutes';
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
 
 export default function LandingWrapper({ children }: Readonly<{ children: ReactNode }>) {
     const hidden = useHiddenRoutes();
     return (
-        <>
+        <SessionProvider >
             {hidden && <Header />}
             {children}
             {hidden && <Footer />}
-        </>
+        </SessionProvider>
     );
 }
