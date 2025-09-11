77e716cd94d86766df2134935269570eb397108b
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-11 20:45:01 +0530

    Loader added to Site

--- src/components/ui/landing/LandingWrapper.tsx
+++ src/components/ui/landing/LandingWrapper.tsx
@@ -3,11 +3,23 @@
 import { useHiddenRoutes } from '@/hooks/useHiddenRoutes';
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
-import { ReactNode } from 'react';
-import { SessionProvider } from 'next-auth/react';
+import { ReactNode, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useUI } from '@/context/UIContext';
 
-export default function LandingWrapper({ children }: Readonly<{ children: ReactNode }>) {
+export default function LandingWrapper({ children }: { children: ReactNode }) {
     const hidden = useHiddenRoutes();
+    const { setShowLoader } = useUI();
+    const { status } = useSession();
+
+    useEffect(() => {
+        if (status === 'loading') setShowLoader(true);
+        if (status === 'authenticated') setShowLoader(false);
+
+        const timer = setTimeout(() => setShowLoader(false), 3000);
+        return () => clearTimeout(timer);
+    }, [status, setShowLoader]);
+
     return (
         <>
             {hidden && <Header />}
