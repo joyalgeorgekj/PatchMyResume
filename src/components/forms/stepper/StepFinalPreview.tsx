0048d41d39257d5136882b18dea11d43bfcc1cf0
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-14 17:55:47 +0530

    Basic JSON Editor added to FInal Step

--- src/components/forms/stepper/StepFinalPreview.tsx
+++ src/components/forms/stepper/StepFinalPreview.tsx
@@ -1,3 +1,8 @@
-export default function StepFinalPreview() {
-    return <>Hey</>;
+import { ResumeDataType } from "@/data/constants/types";
+import JsonEditor from "../JsonEditor";
+import { Dispatch, SetStateAction } from "react";
+
+export default function StepFinalPreview({final, setFinal}: {final: ResumeDataType, setFinal: Dispatch<SetStateAction<ResumeDataType>>}) {
+
+    return <JsonEditor resumeUserData={final} setResumeUserData={setFinal} />;
 }
