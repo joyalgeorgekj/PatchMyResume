71b2649de469ca9cc0630fd884ff82e0bf48325f
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-17 21:17:14 +0530

    Simpler appwrite functions

--- src/lib/server/appwrite.ts
+++ src/lib/server/appwrite.ts
@@ -1,12 +1,33 @@
 import { Client, Databases } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
+const DATABASE_ID = process.env.APPWRITE_DB_ID!;
+const COLLECTION_ID = process.env.APPWRITE_COLLECTION_ID!;
+const ENDPOINT = process.env.APPWRITE_ENDPOINT!
+const PROJECT_ID = process.env.APPWRITE_PROJECT_ID
+const API_KEY = process.env.APPWRITE_API_KEY!
 
 export function getAppwriteClient() {
     const client = new Client()
-        .setEndpoint(process.env.APPWRITE_ENDPOINT!)
-        .setProject(process.env.APPWRITE_PROJECT_ID!)
-        .setKey(process.env.APPWRITE_API_KEY!); // only safe in server
+        .setEndpoint(ENDPOINT!)
+        .setProject(PROJECT_ID!)
+        .setKey(API_KEY!);
 
     return {
         databases: new Databases(client),
     };
+}
+
+export async function getResumeByEmail(email: string) {
+    const { databases } = getAppwriteClient();
+    return databases.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal('email', email)]);
+}
+
+export async function createResumeDoc(email: string, data: any) {
+    const { databases } = getAppwriteClient();
+    return databases.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), { email, ...data });
+}
+
+export async function updateResumeDoc(docId: string, data: any) {
+    const { databases } = getAppwriteClient();
+    return databases.updateDocument(DATABASE_ID, COLLECTION_ID, docId, data);
 }
