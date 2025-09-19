c6b8ab64a5eda90b250874f6d037d91ee962dc39
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-19 16:26:25 +0530

    Added Empty section placeholder

--- src/components/forms/stepper/StepAiSuggestions.tsx
+++ src/components/forms/stepper/StepAiSuggestions.tsx
@@ -19,7 +19,7 @@ export default function StepAiSuggestions({
 }) {
     const [selected, setSelected] = useState<{ [section: string]: string[] }>({});
     useEffect(() => {
-        console.log(selected);
+        console.log('selected', Object.keys(selected)[0]);
     });
 
     function resumeUserDataUpdator<SectionType>(
@@ -112,117 +112,127 @@ export default function StepAiSuggestions({
                     <div key={section} className="space-y-4">
                         <h3 className="text-lg font-medium capitalize">{section}</h3>
                         <div className="flex flex-col gap-4">
-                            {options.map((opt, idx) => (
+                            {options.length === 0 ? (
                                 <div
-                                    className="grid h-fit grid-cols-[10px_1fr_1fr] gap-4"
+                                    className="flex justify-center items-center w-full aspect-[4/1] border-dashed border-2 border-dark-muted/50 rounded-4xl capitalize"
                                     style={{ height: 'fit-content' }}
-                                    key={idx}
+                                    key={ind}
                                 >
-                                    <p>{idx + 1}</p>
-                                    <div
-                                        className={`h-fit rounded-2xl border p-4 shadow-sm transition ${
-                                            section === Object.keys(selected)[ind] &&
-                                            selected[section][idx] === opt.option1
-                                                ? 'border-secondary'
-                                                : 'border-light bg-light-muted'
-                                        }`}
-                                    >
-                                        {section === Object.keys(selected)[ind] &&
-                                            selected[section][idx] === opt.option1 && (
-                                                <span className="border-dark-muted/45 bg-light text-dark inline-flex items-center rounded-full border px-3 py-1 text-xs font-medium">
-                                                    Selected
-                                                </span>
-                                            )}
-                                        <p className="text-dark my-2 text-sm font-medium text-wrap whitespace-pre-wrap">
-                                            {opt.option1}
-                                        </p>
-                                        <div className="flex gap-2">
-                                            <button
-                                                className="border-secondary-muted text-dark shadow-secoborder-secondary-muted flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-sm font-medium shadow-sm transition hover:scale-95"
-                                                onClick={() => {
-                                                    updateFinal(section, opt.option1, idx);
-                                                    setSelected((prev) => {
-                                                        if (prev[section]) {
-                                                            let updated = prev[section];
-                                                            updated[idx] = opt.option1;
-                                                            return {
-                                                                ...prev,
-                                                                [section]: updated,
-                                                            };
-                                                        } else {
-                                                            return {
-                                                                ...prev,
-                                                                [section]: [opt.option1],
-                                                            };
-                                                        }
-                                                    });
-                                                }}
-                                            >
-                                                Use This
-                                            </button>
-                                            <button
-                                                className="border-dark-muted/30 hover:bg-light-muted shadow-dark-muted flex w-fit cursor-pointer items-center justify-center gap-2 rounded-md border px-4 py-2 text-sm font-medium shadow-xs transition hover:scale-95"
-                                                onClick={() =>
-                                                    console.log('Edit clicked', section, opt)
-                                                }
-                                            >
-                                                Edit
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <div
-                                        className={`h-fit rounded-2xl border p-4 shadow-sm transition ${
-                                            section === Object.keys(selected)[ind] &&
-                                            selected[section][idx] === opt.option2
-                                                ? 'border-secondary'
-                                                : 'border-light bg-light-muted'
-                                        }`}
-                                    >
-                                        {section === Object.keys(selected)[ind] &&
-                                            selected[section][idx] === opt.option2 && (
-                                                <span className="border-dark-muted/45 bg-light text-dark inline-flex items-center rounded-full border px-3 py-1 text-xs font-medium">
-                                                    Selected
-                                                </span>
-                                            )}
-                                        <p className="text-dark my-2 text-sm font-medium text-wrap whitespace-pre-wrap">
-                                            {opt.option2}
-                                        </p>
-                                        <div className="flex gap-2">
-                                            <button
-                                                className="border-secondary-muted text-dark shadow-secoborder-secondary-muted flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-sm font-medium shadow-sm transition hover:scale-95"
-                                                onClick={() => {
-                                                    updateFinal(section, opt.option2, idx);
-                                                    setSelected((prev) => {
-                                                        if (prev[section]) {
-                                                            let updated = prev[section];
-                                                            updated[idx] = opt.option2;
-                                                            return {
-                                                                ...prev,
-                                                                [section]: updated,
-                                                            };
-                                                        } else {
-                                                            return {
-                                                                ...prev,
-                                                                [section]: [opt.option2],
-                                                            };
-                                                        }
-                                                    });
-                                                }}
-                                            >
-                                                Use This
-                                            </button>
-                                            <button
-                                                className="border-dark-muted/30 hover:bg-light-muted shadow-dark-muted flex w-fit cursor-pointer items-center justify-center gap-2 rounded-md border px-4 py-2 text-sm font-medium shadow-xs transition hover:scale-95"
-                                                onClick={() =>
-                                                    console.log('Edit clicked', section, opt)
-                                                }
-                                            >
-                                                Edit
-                                            </button>
-                                        </div>
-                                    </div>
+                                    Empty {section} Section
                                 </div>
-                            ))}
+                            ) : (
+                                options.map((opt, idx) => (
+                                    <div
+                                        className="grid h-fit grid-cols-[10px_1fr_1fr] gap-4"
+                                        style={{ height: 'fit-content' }}
+                                        key={idx}
+                                    >
+                                        <p>{idx + 1}</p>
+                                        <div
+                                            className={`h-fit rounded-2xl border p-4 shadow-sm transition ${
+                                                section === Object.keys(selected)[ind] &&
+                                                selected[section][idx] === opt.option1
+                                                    ? 'border-secondary'
+                                                    : 'border-light bg-light-muted'
+                                            }`}
+                                        >
+                                            {section === Object.keys(selected)[ind] &&
+                                                selected[section][idx] === opt.option1 && (
+                                                    <span className="border-dark-muted/45 bg-light text-dark inline-flex items-center rounded-full border px-3 py-1 text-xs font-medium">
+                                                        Selected
+                                                    </span>
+                                                )}
+                                            <p className="text-dark my-2 text-sm font-medium text-wrap whitespace-pre-wrap">
+                                                {opt.option1}
+                                            </p>
+                                            <div className="flex gap-2">
+                                                <button
+                                                    className="border-secondary-muted text-dark shadow-secoborder-secondary-muted flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-sm font-medium shadow-sm transition hover:scale-95"
+                                                    onClick={() => {
+                                                        updateFinal(section, opt.option1, idx);
+                                                        setSelected((prev) => {
+                                                            if (prev[section]) {
+                                                                let updated = prev[section];
+                                                                updated[idx] = opt.option1;
+                                                                return {
+                                                                    ...prev,
+                                                                    [section]: updated,
+                                                                };
+                                                            } else {
+                                                                return {
+                                                                    ...prev,
+                                                                    [section]: [opt.option1],
+                                                                };
+                                                            }
+                                                        });
+                                                    }}
+                                                >
+                                                    Use This
+                                                </button>
+                                                <button
+                                                    className="border-dark-muted/30 hover:bg-light-muted shadow-dark-muted flex w-fit cursor-pointer items-center justify-center gap-2 rounded-md border px-4 py-2 text-sm font-medium shadow-xs transition hover:scale-95"
+                                                    onClick={() =>
+                                                        console.log('Edit clicked', section, opt)
+                                                    }
+                                                >
+                                                    Edit
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div
+                                            className={`h-fit rounded-2xl border p-4 shadow-sm transition ${
+                                                section === Object.keys(selected)[ind] &&
+                                                selected[section][idx] === opt.option2
+                                                    ? 'border-secondary'
+                                                    : 'border-light bg-light-muted'
+                                            }`}
+                                        >
+                                            {section === Object.keys(selected)[ind] &&
+                                                selected[section][idx] === opt.option2 && (
+                                                    <span className="border-dark-muted/45 bg-light text-dark inline-flex items-center rounded-full border px-3 py-1 text-xs font-medium">
+                                                        Selected
+                                                    </span>
+                                                )}
+                                            <p className="text-dark my-2 text-sm font-medium text-wrap whitespace-pre-wrap">
+                                                {opt.option2}
+                                            </p>
+                                            <div className="flex gap-2">
+                                                <button
+                                                    className="border-secondary-muted text-dark shadow-secoborder-secondary-muted flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-sm font-medium shadow-sm transition hover:scale-95"
+                                                    onClick={() => {
+                                                        updateFinal(section, opt.option2, idx);
+                                                        setSelected((prev) => {
+                                                            if (prev[section]) {
+                                                                let updated = prev[section];
+                                                                updated[idx] = opt.option2;
+                                                                return {
+                                                                    ...prev,
+                                                                    [section]: updated,
+                                                                };
+                                                            } else {
+                                                                return {
+                                                                    ...prev,
+                                                                    [section]: [opt.option2],
+                                                                };
+                                                            }
+                                                        });
+                                                    }}
+                                                >
+                                                    Use This
+                                                </button>
+                                                <button
+                                                    className="border-dark-muted/30 hover:bg-light-muted shadow-dark-muted flex w-fit cursor-pointer items-center justify-center gap-2 rounded-md border px-4 py-2 text-sm font-medium shadow-xs transition hover:scale-95"
+                                                    onClick={() =>
+                                                        console.log('Edit clicked', section, opt)
+                                                    }
+                                                >
+                                                    Edit
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            )}
                         </div>
                     </div>
                 ))}
