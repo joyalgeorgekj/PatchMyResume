33bdbf49160809c2d120719b3d8e59b7228abd18
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-09-28 17:09:07 +0530

    Added disable attribute and style

--- src/components/forms/AuthButton.tsx
+++ src/components/forms/AuthButton.tsx
@@ -2,7 +2,7 @@ import { useUI } from '@/context/UIContext';
 import { AuthListType } from '@/data/constants/types';
 import { signIn } from 'next-auth/react';
 
-export default function AuthButton({ Icon, label }: AuthListType) {
+export default function AuthButton({ Icon, label, disable = false }: AuthListType) {
     const { setLoader } = useUI();
 
     const handleLogin = () => {
@@ -13,7 +13,8 @@ export default function AuthButton({ Icon, label }: AuthListType) {
     return (
         <button
             onClick={handleLogin}
-            className="border-primary-muted text-dark shadow-primary-muted flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-sm font-medium shadow-sm transition hover:scale-105"
+            disabled={disable}
+            className="border-primary-muted disabled:border-primary-muted/50 disabled:scale-100 disabled:cursor-not-allowed disabled:text-dark-muted/75 text-dark shadow-primary-muted flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 px-4 py-2 text-sm font-medium shadow-sm transition hover:scale-105 md:text-base text-[12px]"
         >
             <Icon className="text-lg" /> {label}
         </button>
